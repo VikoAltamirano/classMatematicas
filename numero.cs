6ebc0f7cf09e2a2f7783e0f642256b13a47cccbd
VikoAltamirano <vikoaltamirano248@gmail.com>
2022-10-27 19:37:40 -0500

    Create class numero.cs

---
+++ numero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Numero
+{
+	private int num1;
+    private int num2;
+	public Numero()
+	{
+	}
+
+    public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+    public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+
+	public int Suma()
+    {
+        return num1 + num2;
+    }
+}
